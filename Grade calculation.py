1bcdddf128b87e778d97ceb6f0d89821fdcb62e4
Guts <131848762+GutsGoraphatPhotgudan@users.noreply.github.com>
2023-05-23 11:15:29 +0700

    Create Grade calculation.py

---
+++ Grade calculation.py
@@ -0,0 +1,18 @@
+Grades = int(input("Please input grade(0 to stop):")
+while Grades != 0:
+         if Grades <= 80:
+             print("A")
+         elif Grades <= 70:
+             print("B")
+         elif Grades <= 60:
+             print("C")
+         elif Grades <= 50:
+             print("D")
+         else:
+             print("F")
+             
+             
+
+             
+
+        
